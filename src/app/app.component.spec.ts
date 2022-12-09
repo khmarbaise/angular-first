9478dde3a53005d0aca5c29d26479b740844bcc2
Karl Heinz Marbaise <kama@soebes.de>
2022-12-09 18:16:59 +0100

    Intentionally failing IT.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-first' Testing.`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-first');
+    expect(app.title).toEqual('angular-firstX');
   });
 
   it('should render title', () => {
